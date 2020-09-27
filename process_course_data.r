c4cc2e61f11b70798b59025848b346e5a197fbfe
Graham Bearden <gbearden@users.noreply.github.com>
2020-09-27 13:44:39 -0700

    Update process_course_data.r

--- process_course_data.r
+++ process_course_data.r
@@ -1,40 +1,55 @@
 library(tidyverse)
 
-old_contribution_data <- read_csv('https://goo.gl/GTRqZs')
-new_contribution_data <- read_csv('Contributions_to_Candidates_and_Political_Committees.csv')
-
-old_crime_data <- read_csv('https://goo.gl/FHW2Ni')
-new_crime_data <- read_csv('Crime_Data.csv')
-
-new_contribution_data %>% 
-  select_(.dots = colnames(select(old_contribution_data, -receipt_year))) %>% 
-  filter(complete.cases(.)) %>% 
-  sample_n(90000) %>% 
-  gather(var, val, 2:21) %>% 
-  mutate(val = ifelse(runif(nrow(.), 0, 1) > .1, val, NA)) %>% 
-  spread(var, val) %>% 
-  mutate(
-    receipt_date   = lubridate::mdy(receipt_date)
-    , receipt_year = lubridate::year(receipt_date)
+read_csv('Best_mask_hospitalization_all_locs.csv') %>% 
+  select(-contains('upper'), -contains('lower'), -contains('smooth'), -V1, -location_id, -sero_pct) %>%
+  rename(
+    location                = location_name
+    , population            = total_pop
+    , infections_cumulative = inf_cuml_mean
+    , total_deaths          = totdea_mean
+    , total_deaths_p100k    = totdea_mean_p100k_rate 
     ) %>% 
-  write.table('contributions_data.txt')
+  rename_all(function(.) gsub('_mean|_rate', '', .)) %>%
+  write.table('covid_data.txt')
 
-new_crime_data <- new_crime_data %>% 
-  rename_all(tolower)
+new_crime_data <- read_csv('SPD_Crime_Data__2008-Present.csv') %>% 
+  rename_all(tolower) %>% 
+  rename(
+    neighborhood = mcpp
+    , occurred_date = `offense start datetime`
+    , reported_date = `report datetime`
+    ) %>% 
+  select(-longitude, -latitude, -`offense end datetime`
+         , -`group a b`, -`offense code`, -`100 block address`
+         , -sector 
+         ) %>% 
+  mutate(
+    precinct = case_when(precinct %in% 'W'       ~ 'West'
+                           , precinct %in% 'N'   ~ 'North'
+                           , precinct %in% 'S'   ~ 'South'
+                           , precinct %in% 'E'   ~ 'East'
+                           , precinct %in% 'OOJ' ~ 'Out of Jurisdiction'
+                           , precinct %in% 'SW'  ~ 'Southwest'
+                           , TRUE                ~ precinct)
+    )
 
 names(new_crime_data) <- str_replace_all(names(new_crime_data), c(" " = "_"))
 
-new_crime_data %>% 
-  select_(.dots = colnames(select(old_crime_data, -occurred_year, -reported_year))) %>%
+new_crime_data %>%
   filter(complete.cases(.)) %>% 
-  sample_n(190000) %>%
-  gather(var, val, 2:11) %>% 
-  mutate(val = ifelse(runif(nrow(.), 0, 1) > .1, val, NA)) %>% 
-  spread(var, val) %>% 
+  sample_n(200000) %>% 
+  gather(var, val, 5:10) %>%
+  mutate(val = ifelse(runif(nrow(.), 0, 1) > .1, val, NA)) %>%
+  spread(var, val) %>%
   mutate(
-    reported_date   = lubridate::mdy(reported_date)
-    , occurred_date = lubridate::mdy(occurred_date)
+    occurred_time = occurred_date %>% substr(12, 16) %>% str_replace_all('\\:', '')
+    , reported_time = reported_date %>% substr(12, 16) %>% str_replace_all('\\:', '')
+    , occurred_date = occurred_date %>% substr(1, 10) %>% lubridate::ymd()
+    , reported_date = reported_date %>% substr(1, 10) %>% lubridate::ymd()
     , occurred_year = lubridate::year(occurred_date)
     , reported_year = lubridate::year(reported_date)
   ) %>% 
+  select(report_number, offense_id, neighborhood, beat, precinct
+         , crime_against_category, offense, offense_parent_group
+         , contains('occurred'), contains('reported')) %>% 
   write.table('crime_data.txt')
