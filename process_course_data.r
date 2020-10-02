7a6cb39f35e0ea112663efdfbf6345b0266a1ef1
Graham Bearden <gbearden@users.noreply.github.com>
2020-10-02 10:55:33 -0700

    Update process_course_data.r

--- process_course_data.r
+++ process_course_data.r
@@ -1,19 +1,36 @@
 
 library(tidyverse)
 
-read_csv('Best_mask_hospitalization_all_locs.csv') %>% 
+location_rm <- c("Azad Jammu & Kashmir", "Balochistan", "Gilgit-Baltistan", "Islamabad Capital Territory"
+  , "Khyber Pakhtunkhwa","Sindh","Andalucia","Aragon","Cantabria","Castilla-La Mancha"
+  , "Community of Madrid", "Extremadura", "Balearic Islands", "Canary Islands"
+  , "Asturias", "Murcia", "Castile and León", "Catalonia", "Navarre"
+  , "Valencian Community", "Galicia", "Basque Country", "La Rioja", "Baden-Württemberg"
+  , "Bavaria","Berlin", "Brandenburg", "Bremen", "Hamburg", "Hesse", "Lower Saxony"
+  , "Mecklenburg-Vorpommern", "North Rhine-Westphalia","Rhineland-Palatinate", "Saarland"                                        
+  , "Saxony-Anhalt", "Saxony","Schleswig-Holstein", "Thuringia"
+  , "Acre", "Alagoas", "Amazonas", "Amapá", "Bahia", "Ceará", "Distrito Federal"
+  , "Espírito Santo", "Goiás", "Maranhão", "Minas Gerais", "Mato Grosso do Sul", "Mato Grosso"                                     
+  , "Pará", "Paraíba", "Paraná", "Pernambuco", "Piaui", "Rio de Janeiro"                                  
+  , "Rio Grande do Norte", "Rondônia", "Roraima", "Rio Grande do Sul", "Santa Catarina", "Sergipe"                                         
+  , "São Paulo", "Tocantins", "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh", "Delhi"                                           
+  , "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jammu & Kashmir and Ladakh", "Jharkhand"                                       
+  , "Karnataka", "Kerala" )
+
+read_csv('/Users/graham/Downloads/2020_09_18/Best_mask_hospitalization_all_locs.csv') %>% 
   select(-contains('upper'), -contains('lower'), , -V1, -location_id, -contains('smooth')
-         , -sero_pct, -totdea_mean, -totdea_mean_p100k_rate, -contains('mobility')
+         , -sero_pct, -totdea_mean, -totdea_mean_p100k_rate, 
          , -inf_cuml_mean
-         ) %>%
+         ) %>% 
   rename(
     location                = location_name
     , population            = total_pop
     ) %>% 
   rename_all(function(.) gsub('_mean|_rate', '', .)) %>% 
-  write_csv('covid_data.csv')
+  filter(! location %in% location_rm) %>% 
+  write_csv('/Users/graham/Downloads/2020_09_18/covid_data.csv', na = '')
 
-new_crime_data <- read_csv('SPD_Crime_Data__2008-Present.csv') %>% 
+new_crime_data <- read_csv('/Users/graham/Downloads/SPD_Crime_Data__2008-Present.csv') %>% 
   rename_all(tolower) %>% 
   rename(
     neighborhood = mcpp
@@ -53,4 +70,4 @@ new_crime_data %>%
   select(report_number, offense_id, neighborhood, beat, precinct
          , crime_against_category, offense, offense_parent_group
          , contains('occurred'), contains('reported')) %>% 
-  write_csv('crime_data.csv')
+  write_csv('/Users/graham/Downloads/2020_09_18/crime_data.csv', na = '')
