f6d5686eed83f1a56ebd8db8151310b14fb1027e
graham-bearden-kinsa <graham.bearden@kinsahealth.com>
2022-10-15 12:17:05 -0700

    add example plots

---
+++ plots/example_plots.R
@@ -0,0 +1,51 @@
+
+# -------------------------------
+# Colombia Tempterature Line Plot
+# -------------------------------
+
+colombia %>%
+  filter(year >= 1850) %>% 
+  group_by(year, city) %>% 
+  summarise(across(fahrenheit, mean), .groups = 'drop') %>% 
+  ggplot(aes(x = year, y = fahrenheit, color = city)) + 
+  geom_line(size = 1) + 
+  theme_minimal() + 
+  labs(
+    x = 'Year', 
+    y = 'Temperature', 
+    title = 'Average Temperatures in Bogota and Cali since 1850',
+    color = NULL
+    ) + 
+  theme(legend.position = 'top') + 
+  ggthemes::scale_color_few()
+
+ggsave('plots/colombia.jpeg', device = 'jpeg')
+
+# ------------------------
+# Airbnb Price and Ratings
+# ------------------------
+
+airbnb %>% 
+  select(room_type, address, rating, price) %>% 
+  group_by(room_type, address) %>% 
+  summarise(across(.fns = mean, na.rm = TRUE), .groups = 'drop') %>% 
+  gather(metric, value, c(rating, price)) %>% 
+  mutate(across(metric, stringr::str_to_title)) %>% 
+  ggplot(aes(x = address, y = value, fill = room_type)) + 
+  geom_bar(stat = 'identity', position = position_dodge(), alpha = .8) + 
+  facet_wrap(. ~ metric, ncol = 1, scales = 'free_y') + 
+  labs(
+    x = NULL,
+    fill = NULL,
+    y = NULL,
+    title = 'Average Price and Ratings by Address and Room Type',
+    subtitle = 'No meaningful relationship exists between Price and Ratings'
+  ) + 
+  theme_minimal() + 
+  theme(
+    legend.position = 'top',
+    strip.text = element_text(size = 10)
+    ) + 
+  ggthemes::scale_fill_calc()
+  
+ggsave('plots/airbnb_price_ratings.jpeg', device = 'jpeg')
